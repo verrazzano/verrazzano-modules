d11abc4b9602e4c49e14252ce111215c44e1899d
Paul Mackin <paul.mackin@oracle.com>
2023-04-20 20:07:15 -0400

    checkpoint

--- common/lifecycle-actions/helm/overrides.go
+++ common/lifecycle-actions/helm/overrides.go
@@ -4,54 +4,53 @@
 package helm
 
 import (
-	"context"
 	"fmt"
+	moduleplatform "github.com/verrazzano/verrazzano-modules/module-operator/apis/platform/v1alpha1"
 	"github.com/verrazzano/verrazzano/pkg/bom"
 	"github.com/verrazzano/verrazzano/pkg/helm"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/override"
+	vzoverride "github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/override"
+	"os"
 
-	"github.com/Jeffail/gabs/v2"
-	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
-	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	vzos "github.com/verrazzano/verrazzano/pkg/os"
 
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 
-	"k8s.io/api/core/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/yaml"
 )
 
-// buildCustomHelmOverrides Builds the helm overrides for a release, including image and file, and custom overrides
+// BuildCustomHelmOverrides Builds the helm overrides for a release, including image and file, and custom overrides
 // - returns an error and a HelmOverride struct with the field populated
-func buildCustomHelmOverrides(context spi.ComponentContext, namespace string, additionalValues ...bom.KeyValue) ([]helm.HelmOverrides, error) {
+func BuildCustomHelmOverrides(context spi.ComponentContext, namespace string, moduleOverrides moduleplatform.Overrides) ([]helm.HelmOverrides, error) {
 	// Optionally create a second override file.  This will contain both image setOverrides and any additional
 	// setOverrides required by a component.
 	// Get image setOverrides unless opt out
 	var kvs []bom.KeyValue
 	var err error
-	var overrides []helm.HelmOverrides
+	var helmOverrides []helm.HelmOverrides
 
-	// Sort the kvs list by priority (0th term has the highest priority)
+	vzOverrides := v1alpha1.Overrides{
+		ConfigMapRef: moduleOverrides.ConfigMapRef,
+		SecretRef:    moduleOverrides.SecretRef,
+		Values:       moduleOverrides.Values,
+	}
 
 	// Getting user defined Helm overrides as the highest priority
-	overrideStrings, err := override.GetInstallOverridesYAML(context, h.GetOverrides(context.EffectiveCR()).([]v1alpha1.Overrides))
+	overrideStrings, err := vzoverride.GetInstallOverridesYAML(context, []v1alpha1.Overrides{vzOverrides})
 	if err != nil {
-		return overrides, err
+		return nil, err
 	}
 	for _, overrideString := range overrideStrings {
 		file, err := vzos.CreateTempFile(fmt.Sprintf("helm-overrides-user-%s-*.yaml", h.Name()), []byte(overrideString))
 		if err != nil {
 			context.Log().Error(err.Error())
-			return overrides, err
+			return nil, err
 		}
 		kvs = append(kvs, bom.KeyValue{Value: file.Name(), IsFile: true})
 	}
 
 	// Create files from the Verrazzano Helm values
-	newKvs, err := h.filesFromVerrazzanoHelm(context, namespace, additionalValues)
+	newKvs, err := filesFromVerrazzanoHelm(context, namespace, additionalValues)
 	if err != nil {
 		return overrides, err
 	}
@@ -67,135 +66,91 @@ func buildCustomHelmOverrides(context spi.ComponentContext, namespace string, ad
 	return overrides, nil
 }
 
-// GetInstallOverridesYAML takes the list of Overrides and returns a string array of YAMLs
-func GetInstallOverridesYAML(ctx spi.ComponentContext, overrides []v1alpha1.Overrides) ([]string, error) {
-	return getInstallOverridesYAML(ctx.Log(), ctx.Client(), v1alpha1.ConvertValueOverridesToV1Beta1(overrides), ctx.EffectiveCR().Namespace)
-}
+func filesFromVerrazzanoHelm(context spi.ComponentContext, namespace string, additionalValues []bom.KeyValue) ([]bom.KeyValue, error) {
+	var kvs []bom.KeyValue
+	var newKvs []bom.KeyValue
 
-// GetInstallOverridesYAMLUsingClient takes the list of Overrides and returns a string array of YAMLs using the
-// specified client
-func GetInstallOverridesYAMLUsingClient(client client.Client, overrides []v1beta1.Overrides, namespace string) ([]string, error) {
-	// DefaultLogger is used since this is invoked from validateInstall and validateUpdate functions and
-	// any actual logging isn't being performed
-	log := vzlog.DefaultLogger()
-	return getInstallOverridesYAML(log, client, overrides, namespace)
-}
-
-// ExtractValueFromOverrideString is a helper function to extract a given value from override.
-func ExtractValueFromOverrideString(overrideStr string, field string) (interface{}, error) {
-	jsonConfig, err := yaml.YAMLToJSON([]byte(overrideStr))
+	// Get image overrides if they are specified
+	imageOverrides, err := getImageOverrides(h.ReleaseName)
 	if err != nil {
-		return nil, err
+		return newKvs, err
 	}
-	jsonString, err := gabs.ParseJSON(jsonConfig)
-	if err != nil {
-		return nil, err
-	}
-	return jsonString.Path(field).Data(), nil
-}
+	kvs = append(kvs, imageOverrides...)
 
-// getInstallOverridesYAML takes the list of Overrides and returns a string array of YAMLs
-func getInstallOverridesYAML(log vzlog.VerrazzanoLogger, client client.Client, overrides []v1beta1.Overrides,
-	namespace string) ([]string, error) {
-	var overrideStrings []string
-	for _, override := range overrides {
-		// Check if ConfigMapRef is populated and gather data
-		if override.ConfigMapRef != nil {
-			// Get the ConfigMap data
-			data, err := getConfigMapOverrides(log, client, override.ConfigMapRef, namespace)
-			if err != nil {
-				return overrideStrings, err
-			}
-			overrideStrings = append(overrideStrings, data)
+	// Append any additional setOverrides for the component (see Keycloak.go for example)
+	if h.AppendOverridesFunc != nil {
+		overrideValues, err := h.AppendOverridesFunc(context, h.ReleaseName, namespace, h.ChartDir, []bom.KeyValue{})
+		if err != nil {
+			return newKvs, err
+		}
+		kvs = append(kvs, overrideValues...)
+	}
+
+	// Append any special overrides passed in
+	if len(additionalValues) > 0 {
+		kvs = append(kvs, additionalValues...)
+	}
+
+	// Expand the existing kvs values into expected format
+	var fileValues []bom.KeyValue
+	for _, kv := range kvs {
+		// If the value is a file, add it to the new kvs
+		if kv.IsFile {
+			newKvs = append(newKvs, kv)
 			continue
 		}
-		// Check if SecretRef is populated and gather data
-		if override.SecretRef != nil {
-			// Get the Secret data
-			data, err := getSecretOverrides(log, client, override.SecretRef, namespace)
+
+		// If set file, extract the data into the value parameter
+		if kv.SetFile {
+			data, err := os.ReadFile(kv.Value)
 			if err != nil {
-				return overrideStrings, err
+				return newKvs, context.Log().ErrorfNewErr("Could not open file %s: %v", kv.Value, err)
 			}
-			overrideStrings = append(overrideStrings, data)
-			continue
-		}
-		if override.Values != nil {
-			overrideValuesData, err := yaml.Marshal(override.Values)
-			if err != nil {
-				return overrideStrings, err
-			}
-			overrideStrings = append(overrideStrings, string(overrideValuesData))
+			kv.Value = string(data)
 		}
 
+		fileValues = append(fileValues, kv)
 	}
-	return overrideStrings, nil
+
+	// Take the YAML values and construct a YAML file
+	// This uses the Helm YAML formatting
+	fileString, err := yaml.HelmValueFileConstructor(fileValues)
+	if err != nil {
+		return newKvs, context.Log().ErrorfNewErr("Could not create YAML file from key value pairs: %v", err)
+	}
+
+	// Create the file from the string
+	if len(fileString) > 0 {
+		file, err := vzos.CreateTempFile(fmt.Sprintf("helm-overrides-verrazzano-%s-*.yaml", h.Name()), []byte(fileString))
+		if err != nil {
+			context.Log().Error(err.Error())
+			return newKvs, err
+		}
+		if file != nil {
+			newKvs = append(newKvs, bom.KeyValue{Value: file.Name(), IsFile: true})
+		}
+	}
+	return newKvs, nil
 }
 
-// getConfigMapOverrides takes a ConfigMap selector and returns the YAML data and handles k8s api errors appropriately
-func getConfigMapOverrides(log vzlog.VerrazzanoLogger, client client.Client, selector *v1.ConfigMapKeySelector,
-	namespace string) (string, error) {
-	configMap := &v1.ConfigMap{}
-	nsn := types.NamespacedName{Name: selector.Name, Namespace: namespace}
-	optional := selector.Optional
-	err := client.Get(context.TODO(), nsn, configMap)
-	if err != nil {
-		if !k8sErrors.IsNotFound(err) {
-			log.Errorf("Error retrieving ConfigMap %s: %v", nsn.Name, err)
-			return "", err
+// organizeHelmOverrides creates a list of Helm overrides from key value pairs in reverse precedence (0th value has the lowest precedence)
+// Each key value pair gets its own override object to keep strict precedence
+func organizeHelmOverrides(kvs []bom.KeyValue) []helm.HelmOverrides {
+	var overrides []helm.HelmOverrides
+	for _, kv := range kvs {
+		if kv.SetString {
+			// Append in reverse order because helm precedence is right to left
+			overrides = append([]helm.HelmOverrides{{SetStringOverrides: fmt.Sprintf("%s=%s", kv.Key, kv.Value)}}, overrides...)
+		} else if kv.SetFile {
+			// Append in reverse order because helm precedence is right to left
+			overrides = append([]helm.HelmOverrides{{SetFileOverrides: fmt.Sprintf("%s=%s", kv.Key, kv.Value)}}, overrides...)
+		} else if kv.IsFile {
+			// Append in reverse order because helm precedence is right to left
+			overrides = append([]helm.HelmOverrides{{FileOverride: kv.Value}}, overrides...)
+		} else {
+			// Append in reverse order because helm precedence is right to left
+			overrides = append([]helm.HelmOverrides{{SetOverrides: fmt.Sprintf("%s=%s", kv.Key, kv.Value)}}, overrides...)
 		}
-		if optional == nil || !*optional {
-			err = log.ErrorfThrottledNewErr("Could not get Configmap %s from namespace %s: %v", nsn.Name, nsn.Namespace, err)
-			return "", err
-		}
-		log.Infof("Optional Configmap %s from namespace %s not found", nsn.Name, nsn.Namespace)
-		return "", nil
 	}
-
-	// Get resource data
-	fieldData, ok := configMap.Data[selector.Key]
-	if !ok {
-		if optional == nil || !*optional {
-			err := log.ErrorfThrottledNewErr("Could not get Data field %s from Resource %s from namespace %s", selector.Key, nsn.Name, nsn.Namespace)
-			return "", err
-		}
-		log.Infof("Optional Resource %s from namespace %s missing Data key %s", nsn.Name, nsn.Namespace, selector.Key)
-	}
-	return fieldData, nil
-}
-
-// getSecretOverrides takes a Secret selector and returns the YAML data and handles k8s api errors appropriately
-func getSecretOverrides(log vzlog.VerrazzanoLogger, client client.Client, selector *v1.SecretKeySelector,
-	namespace string) (string, error) {
-	sec := &v1.Secret{}
-	nsn := types.NamespacedName{Name: selector.Name, Namespace: namespace}
-	optional := selector.Optional
-	err := client.Get(context.TODO(), nsn, sec)
-	if err != nil {
-		if !k8sErrors.IsNotFound(err) {
-			log.Errorf("Error retrieving Secret %s: %v", nsn.Name, err)
-			return "", err
-		}
-		if optional == nil || !*optional {
-			err = log.ErrorfThrottledNewErr("Could not get Secret %s from namespace %s: %v", nsn.Name, nsn.Namespace, err)
-			return "", err
-		}
-		log.Infof("Optional Secret %s from namespace %s not found", nsn.Name, nsn.Namespace)
-		return "", nil
-	}
-
-	dataStrings := map[string]string{}
-	for key, val := range sec.Data {
-		dataStrings[key] = string(val)
-	}
-
-	// Get resource data
-	fieldData, ok := dataStrings[selector.Key]
-	if !ok {
-		if optional == nil || !*optional {
-			err := log.ErrorfThrottledNewErr("Could not get Data field %s from Resource %s from namespace %s", selector.Key, nsn.Name, nsn.Namespace)
-			return "", err
-		}
-		log.Infof("Optional Resource %s from namespace %s missing Data key %s", nsn.Name, nsn.Namespace, selector.Key)
-	}
-	return fieldData, nil
+	return overrides
 }
