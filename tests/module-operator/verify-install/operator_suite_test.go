6c7b76459987356caf39aba6ddca149b7056b9ad
Ravi Singhal <ravi.r.singhal@oracle.com>
2023-05-23 17:33:16 +0530

    fix test

--- tests/module-operator/verify-install/operator_suite_test.go
+++ tests/module-operator/verify-install/operator_suite_test.go
@@ -3,29 +3,16 @@
 package operator_test
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 	"github.com/stretchr/testify/suite"
-	"github.com/verrazzano/verrazzano-modules/pkg/k8sutil"
 )
 
 type OperatorTestSuite struct {
 	suite.Suite
 	gomega types.Gomega
-}
-
-func (suite *OperatorTestSuite) SetupSuite() {
-	err := k8sutil.NewYAMLApplier(nil, "").ApplyFTDefaultConfig(fmt.Sprintf("%s/%s", os.Getenv("VMO_ROOT"), "build/deploy/verrazzano-module-operator.yaml"), nil)
-	suite.gomega.Expect(err).Should(gomega.BeNil())
-}
-
-func (suite *OperatorTestSuite) TearDownSuite() {
-	err := k8sutil.NewYAMLApplier(nil, "").DeleteFTDefaultConfig(fmt.Sprintf("%s/%s", os.Getenv("VMO_ROOT"), "build/deploy/verrazzano-module-operator.yaml"), nil)
-	suite.gomega.Expect(err).Should(gomega.BeNil())
 }
 
 func TestOperatorTestSuite(t *testing.T) {
