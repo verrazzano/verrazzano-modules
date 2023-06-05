e17f3a4f7a83eb39ce62b0f17fb586d36d6c5c79
Paul Mackin <paul.mackin@oracle.com>
2023-06-05 14:13:54 -0400

    VZ-9610 add chartutil_test.go and move ccm chart (#128)

--- module-operator/controllers/module/chartutil_test.go
+++ module-operator/controllers/module/chartutil_test.go
@@ -4,13 +4,88 @@
 package module
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	moduleapi "github.com/verrazzano/verrazzano-modules/module-operator/apis/platform/v1alpha1"
+	"github.com/verrazzano/verrazzano-modules/module-operator/internal/config"
+	"path"
+	"testing"
 )
 
-// TestLookupChartLeafDirName tests the lookup of the chart directory name
+// TestLoadHelmInfo tests the loadHelmInfo function
+// GIVEN a Module
+// WHEN the loadHelmInfo is called
+// THEN ensure that the correct HelmInfo is returned
+func TestLoadHelmInfo(t *testing.T) {
+	asserts := assert.New(t)
+
+	const (
+		rootDir = "../../manifests/charts"
+	)
+	tests := []struct {
+		name        string
+		moduleName  string
+		expectedDir string
+		version     string
+	}{
+		{
+			name:        "test-ccm",
+			moduleName:  "oci-ccm",
+			expectedDir: path.Join(rootDir, "modules/oci-ccm/1.25.0"),
+		},
+		{
+			name:        "test-calico",
+			moduleName:  "calico",
+			expectedDir: path.Join(rootDir, "modules/calico/3.25.0"),
+		},
+		{
+			name:        "test-vz-test",
+			moduleName:  "helm",
+			expectedDir: path.Join(rootDir, "vz-test/0.1.0"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			c := config.Get()
+			defer func() { config.Set(c) }()
+			c.ChartsDir = rootDir
+			config.Set(c)
+
+			mod := moduleapi.Module{
+				Spec: moduleapi.ModuleSpec{
+					ModuleName: test.moduleName,
+				},
+			}
+			info, err := loadHelmInfo(&mod)
+			asserts.NoError(err)
+			asserts.NotNil(info)
+		})
+	}
+}
+
+// TestMissingChartFileError tests the loadHelmInfo function where the file is missing
+// GIVEN a Module
+// WHEN the loadHelmInfo is called with the wrong root dir
+// THEN ensure that the correct error is returned
+func TestMissingChartFileError(t *testing.T) {
+	asserts := assert.New(t)
+	c := config.Get()
+	defer func() { config.Set(c) }()
+	c.ChartsDir = "missingdir"
+	config.Set(c)
+
+	mod := moduleapi.Module{
+		Spec: moduleapi.ModuleSpec{
+			ModuleName: "oci-ccm",
+		},
+	}
+	_, err := loadHelmInfo(&mod)
+	asserts.Error(err)
+	asserts.Contains(err.Error(), "NotFound")
+}
+
+// TestLookupChartLeafDirName tests the lookup of the chart leaf directory name
 // GIVEN a Module
 // WHEN the lookupChartLeafDirName is called
 // THEN ensure that the correct directory name is returned
@@ -36,14 +111,14 @@ func TestLookupChartLeafDirName(t *testing.T) {
 		{
 			name:        "test-calico-version",
 			moduleName:  "calico",
-			version:     "v1.26.0",
-			expectedDir: "modules/calico/1.26.0",
+			version:     "v3.25.0",
+			expectedDir: "modules/calico/3.25.0",
 		},
 		{
 			name:        "test-calico-version-no-v",
 			moduleName:  "calico",
-			version:     "1.26.0",
-			expectedDir: "modules/calico/1.26.0",
+			version:     "3.25.0",
+			expectedDir: "modules/calico/3.25.0",
 		},
 		{
 			name:        "test-vz-test",
@@ -74,4 +149,31 @@ func TestLookupChartLeafDirName(t *testing.T) {
 			asserts.Equal(test.expectedDir, dir)
 		})
 	}
+}
+
+// TestLookupChartDir tests the lookup of the chart directory name
+// GIVEN a Module
+// WHEN the lookupChartLeafDirName is called
+// THEN ensure that the correct directory name is returned
+func TestLookupChartDir(t *testing.T) {
+	asserts := assert.New(t)
+	const (
+		rootDir = "/root"
+		calico  = "calico"
+		version = "1.26.0"
+	)
+
+	c := config.Get()
+	defer func() { config.Set(c) }()
+	c.ChartsDir = rootDir
+	config.Set(c)
+
+	mod := moduleapi.Module{
+		Spec: moduleapi.ModuleSpec{
+			ModuleName: calico,
+			Version:    version,
+		},
+	}
+	dir := lookupChartDir(&mod)
+	asserts.Equal(fmt.Sprintf("%s/modules/%s/%s", rootDir, calico, version), dir)
 }
